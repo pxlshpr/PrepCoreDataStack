b2a592e8ae7480acfeaba93bed79eadd9b7b7f32
Ahmed Khalaf <pxlshpr@gmail.com>
2022-11-10 15:46:29 +0500

    tag increment

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
             targets: ["PrepCoreDataStack"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/pxlshpr/PrepDataTypes", from: "0.0.113"),
+        .package(url: "https://github.com/pxlshpr/PrepDataTypes", from: "0.0.135"),
         .package(url: "https://github.com/pxlshpr/SwiftSugar", from: "0.0.85"),
     ],
     targets: [
